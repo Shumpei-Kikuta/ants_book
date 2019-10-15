2a7e85cd20b7e487ce1a935fa54b0b9f66b30612
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-10-15 22:07:38 +0900

    DONE

---
+++ python/beginner/src/carmichael_number.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+
+
+def factorial_devided_n(x: int, n: int, mod: int):
+    res = 1
+    while n > 0:
+        if (n & 1):  # if n has 1 at the least digit
+            res = (res * x) % mod
+        x = x * x
+        n >>= 1
+    return res
+
+
+def carmichael(n: int):
+    for x in range(2, n):
+        nx = factorial_devided_n(x, n, n)
+        if nx % n == x:
+            continue
+        else:
+            return False
+    return True
+
+
+def main():
+    n = int(input())
+    if carmichael(n):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == '__main__':
+    main()
