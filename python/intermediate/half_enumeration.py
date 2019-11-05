4eed33674ad31cfe93a66bab7e566ba7ffca69e4
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-11-05 21:16:12 +0900

    TEST DONE

--- python/intermediate/half_enumeration.py
+++ python/intermediate/half_enumeration.py
@@ -3,12 +3,11 @@ sys.setrecursionlimit(10000000)
 
 
 def preprocess(lists: list) -> list:
-    lists = sorted(lists)
+    lists.sort()
     for i in range(len(lists) - 1):
         if lists[i][1] > lists[i + 1][1]:
-            lists[i + 1][1] = lists[i][1]
+            lists[i + 1] = (lists[i + 1][0], lists[i][1])
     return lists
-
 
 
 def main():
@@ -20,7 +19,8 @@ def main():
         W[i] = w_
     
     n2 = n // 2
-    list1 = []
+    
+    list1 = [None] * (2 ** n2)
     for i in range(1 << n2):
         sum_v = 0
         sum_w = 0
@@ -28,46 +28,35 @@ def main():
             if (i >> j & 1):
                 sum_v += V[j]
                 sum_w += W[j]
-        list1.append([sum_w, sum_v])
+        list1[i] = (sum_w, sum_v)
     
-    list2 = []
+    list2 = [None] * (2 ** (n-n2))
     for i in range(1 << (n-n2)):
-        i = i << n2
         sum_v = 0
         sum_w = 0
-        for j in range(n2, n):
+        for j in range(n - n2):
             if (i >> j & 1):
-                sum_v += V[j]
-                sum_w += W[j]
-        list2.append([sum_w, sum_v])
+                sum_v += V[j + n2]
+                sum_w += W[j + n2]
+        list2[i] = (sum_w, sum_v)
 
     list1 = preprocess(list1)
     list2 = preprocess(list2)
 
-    # print(list1, list2)
     left = 0
     right = len(list2) - 1
     maximum = 0
-    while(left < len(list1)):
-        # print(left, right)
+    for right in range(len(list2) - 1, -1, -1):
         while(True):
             if left == len(list1) - 1:
                 break
             if list1[left + 1][0] + list2[right][0] > w:
                 break
             left += 1
-
-        while(right >= 0):
-            if right == -1:
-                break
-            if list1[left][0] + list2[right][0] <= w:
-                break
-            right -= 1
-        if left > len(list1) or right < 0:
-            break
-        maximum = max(maximum, list1[left][1] + list2[right][1])
-        right -= 1
+        if list1[left][0] + list2[right][0] <= w:
+            maximum = max(maximum, list1[left][1] + list2[right][1])
     print(maximum)
+
 
 if __name__ == '__main__':
     main()
